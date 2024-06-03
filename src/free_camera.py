3d4d4c07543c7c90f7f25fb07d97507828182b68
Sujal Choudhari <sjlchoudhari@gmail.com>
2024-06-03 17:35:21 +0530

    adds: lights and cameras

---
+++ src/free_camera.py
@@ -0,0 +1,68 @@
+import pygame
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from pygame.locals import *
+import numpy as np
+
+class FreeCamera:
+    def __init__(self, position, look_at, up):
+        self.position = np.array(position, dtype=np.float32)
+        self.look_at = np.array(look_at, dtype=np.float32)
+        self.up = np.array(up, dtype=np.float32)
+        self.pitch = 0.0
+        self.yaw = -90.0
+
+        self.front = np.array([0.0, 0.0, -1.0], dtype=np.float32)
+        self.right = np.array([1.0, 0.0, 0.0], dtype=np.float32)
+        self.world_up = np.array(up, dtype=np.float32)
+
+        self.update_camera_vectors()
+
+    def update_camera_vectors(self):
+        front = np.array([
+            np.cos(np.radians(self.yaw)) * np.cos(np.radians(self.pitch)),
+            np.sin(np.radians(self.pitch)),
+            np.sin(np.radians(self.yaw)) * np.cos(np.radians(self.pitch))
+        ], dtype=np.float32)
+
+        self.front = front / np.linalg.norm(front)
+        self.right = np.cross(self.front, self.world_up)
+        self.right /= np.linalg.norm(self.right)
+        self.up = np.cross(self.right, self.front)
+        self.up /= np.linalg.norm(self.up)
+
+    def get_view_matrix(self):
+        return gluLookAt(
+            self.position[0], self.position[1], self.position[2],
+            self.position[0] + self.front[0], self.position[1] + self.front[1], self.position[2] + self.front[2],
+            self.up[0], self.up[1], self.up[2]
+        )
+
+    def process_keyboard(self, direction, delta_time):
+        velocity = 5.0 * delta_time
+        if direction == 'FORWARD':
+            self.position += self.front * velocity
+        if direction == 'BACKWARD':
+            self.position -= self.front * velocity
+        if direction == 'LEFT':
+            self.position -= self.right * velocity
+        if direction == 'RIGHT':
+            self.position += self.right * velocity
+        if direction == 'UP':
+            self.position += self.up * velocity
+        if direction == 'DOWN':
+            self.position -= self.up * velocity
+
+    def process_mouse_movement(self, xoffset, yoffset, sensitivity=0.1):
+        xoffset *= sensitivity
+        yoffset *= sensitivity
+
+        self.yaw += xoffset
+        self.pitch += yoffset
+
+        if self.pitch > 89.0:
+            self.pitch = 89.0
+        if self.pitch < -89.0:
+            self.pitch = -89.0
+
+        self.update_camera_vectors()
---
+++ src/light.py
@@ -0,0 +1,26 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import numpy as np
+from src.geometry.geometry import Geometry
+
+class Light(Geometry):
+    def __init__(self, position, ambient, diffuse, specular, light_id=GL_LIGHT0):
+        super().__init__()
+        self.position = np.array(position)
+        self.ambient = np.array(ambient)
+        self.diffuse = np.array(diffuse)
+        self.specular = np.array(specular)
+        self.light_id = light_id
+
+    def render(self):
+        glEnable(GL_LIGHTING)
+        glEnable(self.light_id)
+
+        glLightfv(self.light_id, GL_POSITION, [self.position[0], self.position[1], self.position[2], 1.0])
+        glLightfv(self.light_id, GL_AMBIENT, self.ambient)
+        glLightfv(self.light_id, GL_DIFFUSE, self.diffuse)
+        glLightfv(self.light_id, GL_SPECULAR, self.specular)
+
+    def disable(self):
+        glDisable(self.light_id)
+        glDisable(GL_LIGHTING)
